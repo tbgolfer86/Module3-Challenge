7596c39bf020501c5142ba0368b8e03d7f4ddaed
tbgolfer86 <tbj86@outlook.com>
2022-12-06 18:50:34 -0500

    concatenated arrays and defined variables in Javascript

--- script.js
+++ script.js
@@ -5,34 +5,47 @@ var lowerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 var upperChoices = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numberChoices = ['0','1','2','3','4','5','6','7','8','9'];
 var specialCharacters = ['!','@','#','$','%','^','&','*','_','-','+','='];
-var chosenCharacters = [];
 
 function generatePassword() {
+  var chosenCharacters = [];
   var passwordLength = prompt("Please enter the length of the password(8-128 characters)");
   if (passwordLength >= 129 || passwordLength <= 7) {
       generatePassword();
   }
   var lowerCase = confirm("Click 'OK' to include lowercase letters in your password");
   if (lowerCase == true) {
-      lowerChoices.concat(chosenCharacters);
+      let lower = chosenCharacters.concat(lowerChoices);
+      console.log(lower);
   }
   var upperCase = confirm("Click 'OK' to include UPPERCASE letters in your password");
   if (upperCase == true) {
-      upperChoices.concat(chosenCharacters, lowerChoices);
+      let upperLower = chosenCharacters.concat(lowerChoices,upperChoices);
+      console.log(upperLower);
   }
   var numbers = confirm("Click OK to include numbers in your password");
   if (numbers == true) {
-      numberChoices.concat(chosenCharacters, lowerChoices, upperChoices);
+      let upperLowerNum = chosenCharacters.concat(lowerChoices,upperChoices,numberChoices);
+      console.log(upperLowerNum);
   }
   var specialChar = confirm("Click 'OK' to include special characters in your password");
   if (specialChar == true) {
-      specialCharacters.concat(chosenCharacters, lowerChoices, upperChoices, numberChoices);
-  }
-  
-  
+      let allChoices = chosenCharacters.concat(lowerChoices,upperChoices,numberChoices,specialCharacters);
+      console.log(allChoices);
+      console.log(allChoices.length)
+    
+      let randomPass = allChoices[Math.floor(Math.random() * allChoices.length)];
+      console.log(randomPass);
 
-  
-  console.log(chosenCharacters);
+      return randomPass;
+  }
+      /*
+        for (let i = 0; i < allChoices.length; i++) {
+        let item = items[i];
+        
+        console.log(item);
+        */
+    
+
 
 // 1. prompt the user for password criteria
 //    a.password length 8 - 128 char
@@ -40,7 +53,7 @@ function generatePassword() {
 // 2. validate the input
 // 3. generate password
 // 4. display password on page
-return "generated password will go here";
+//return randomPass;
 }
 
 
@@ -56,5 +69,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
