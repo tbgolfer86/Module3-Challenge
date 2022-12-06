751ab105c6e346e9b728306d5d21aaf03823f0f5
tbgolfer86 <tbj86@outlook.com>
2022-12-06 16:21:03 -0500

    wrote some Javascript

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperChoices = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberChoices = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = ['!','@','#','$','%','^','&','*','_','-','+','='];
+var chosenCharacters = [];
+
 function generatePassword() {
   var passwordLength = prompt("Please enter the length of the password(8-128 characters)");
+  if (passwordLength >= 129 || passwordLength <= 7) {
+      generatePassword();
+  }
   var lowerCase = confirm("Click 'OK' to include lowercase letters in your password");
+  if (lowerCase == true) {
+      lowerChoices.concat(chosenCharacters);
+  }
   var upperCase = confirm("Click 'OK' to include UPPERCASE letters in your password");
+  if (upperCase == true) {
+      upperChoices.concat(chosenCharacters, lowerChoices);
+  }
   var numbers = confirm("Click OK to include numbers in your password");
+  if (numbers == true) {
+      numberChoices.concat(chosenCharacters, lowerChoices, upperChoices);
+  }
   var specialChar = confirm("Click 'OK' to include special characters in your password");
-  var password ="";
+  if (specialChar == true) {
+      specialCharacters.concat(chosenCharacters, lowerChoices, upperChoices, numberChoices);
+  }
+  
+  
 
-/*
-  const lowerChoice = [abcdefghijklmnopqrstuvwxyz];
-  const upperChoice = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
-  const numbers = [0123456789];
-  const specialChar = [!@#$%^&*_-+=];
-*/
+  
+  console.log(chosenCharacters);
 
 // 1. prompt the user for password criteria
 //    a.password length 8 - 128 char
