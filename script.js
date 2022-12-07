a038610fb05d478fd9bebe003266f3e94b87d366
tbgolfer86 <tbj86@outlook.com>
2022-12-06 23:28:03 -0500

    removed else statements in .js and added comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Usable characters for the password
 var lowerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperChoices = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numberChoices = ['0','1','2','3','4','5','6','7','8','9'];
 var specialCharacters = ['!','@','#','$','%','^','&','*','_','-','+','='];
+
+//user input
 var passwordLength
 var chosenCharacters = [];
 
@@ -17,30 +20,21 @@ function generatePassword() {
   if (lowerCase == true) {
       chosenCharacters = chosenCharacters.concat(lowerChoices);
       console.log(chosenCharacters);
-  } else {
-      chosenCharacters = chosenCharacters;
   }
   upperCase = confirm("Click 'OK' to include UPPERCASE letters in your password");
   if (upperCase == true) {
       chosenCharacters = chosenCharacters.concat(upperChoices);
       console.log(chosenCharacters);
-  } else {
-      chosenCharacters = chosenCharacters;
   }
   numbers = confirm("Click OK to include numbers in your password");
   if (numbers == true) {
       chosenCharacters = chosenCharacters.concat(numberChoices);
       console.log(chosenCharacters);
-  } else {
-      chosenCharacters = chosenCharacters;
-
   }
   specialChar = confirm("Click 'OK' to include special characters in your password");
   if (specialChar == true) {
       chosenCharacters = chosenCharacters.concat(specialCharacters);
       console.log(chosenCharacters);
-  } else {
-    chosenCharacters = chosenCharacters;
   }
 
 } 
